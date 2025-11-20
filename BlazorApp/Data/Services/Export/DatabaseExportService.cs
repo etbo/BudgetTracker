7566a4e31234c7862beba19ab29476a915eee072
Etienne BOULESTEIX <etienne.boulesteix@serac-group.com>
2025-11-20 17:49:39 +0100

    Organisation des pages et components *.razor

--- BlazorApp/Data/Services/Export/DatabaseExportService.cs
+++ BlazorApp/Data/Services/Export/DatabaseExportService.cs
@@ -16,24 +16,17 @@ namespace BlazorApp.Data.Services.Export
             // Localisation du fichier SQLite
             string dbPath = Path.Combine(_env.ContentRootPath, "Database", "BudgetTracker.db");
 
-            Console.WriteLine($"dbPath = {dbPath}");
-
             using var memoryStream = new MemoryStream();
-            Console.WriteLine($"new ZipArchive");
             using (var zip = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
             {
-                Console.WriteLine($"CreateEntry");
                 var entry = zip.CreateEntry("BudgetTracker.db", CompressionLevel.Fastest);
 
-                Console.WriteLine($"Open");
                 using var entryStream = entry.Open();
                 using var fileStream = File.OpenRead(dbPath);
 
-                Console.WriteLine($"Copy stream");
                 fileStream.CopyTo(entryStream);
             }
 
-            Console.WriteLine("Return");
             return memoryStream.ToArray();
         }
     }
